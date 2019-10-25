54ad05e6f6637c9bf730a53967fb794ae5ad232f
Alexandre-CH96 <Alexandre_choque@outlook.com>
2019-10-25 15:09:47 +0200

    mise a jour du script

--- Bash.sh
+++ Bash.sh
@@ -18,7 +18,7 @@ cd ~/Documents
 mkdir monDossier
 # Permet de cree un dossier, Ici, c'est monDossier
 # On peut cree plusiuer dossier en meme temps 
-#Il faudra separer les noms de dossier par un espace
+# Il faudra separer les noms de dossier par un espace
 # Par exemple 
 mkdir dos1 dos2
 
@@ -27,3 +27,33 @@ rm -r *os*
 # -r est obligatoire si on supprime des dossiers 
 # Signifie "n'importe quoi". Ici tout les dossiers 
 # qui contiennent "os"
+
+touch fichier.jpg image.jpg
+# Permet de cree des fichiers, ici, fichier.jpg et image.jpg
+# Image.jpg
+
+cp fichier.jpg ../Images/copyFichier.jpg
+# Permet de copier un ou plusieurs fichier (utilise *)
+# Ici, on copie fichier.jpg dans le dossier Images et
+# on le renome copyFichier (en meme temps)
+
+mv *.jpg ../Images/
+# Permet de déplacer tous les fichiers .jpg pour les placer dans le dossier Images
+
+touch script.sh
+
+chmod ugo-rwx script.sh
+# Permet de changer les permissions
+# User
+# Group
+# Others
+# Les  types de permission sont : 
+# Read
+# Write
+# eXecute
+# Ici, on retire toutes les permissions a tout le monde
+
+chmod u+rwx script.sh
+# Ici, on ajoute les permissions de lecture, ecriture et 
+# execution pour l'utilisateur. Ce fichier  est devenu un 
+# executable (programme)
